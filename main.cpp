0f2e50dd11e4107bc65aadcfcf7b52ec44b291e4
Yudi Yamane <yudiyamane@gmail.com>
2023-07-11 23:16:47 -0300

    refactor: dá nome às veriáveis de custo

--- main.cpp
+++ main.cpp
@@ -8,14 +8,15 @@ using std::endl;
 int main(int argc, char** argv) {
     if (argc != 3) {
         std::printf("Usage: %s \"string1\" \"string2\"", argv[0]);
-        exit(2);
+        exit(1);
     }
 
-    // string t1 = "AGGGCT";
-    // string t2 = "AGGCA";
-    string t1 = argv[1];
-    string t2 = argv[2];
-    SequenceAlingment alignment(t1, t2, 3, 2);
+    string t1         = argv[1];
+    string t2         = argv[2];
+    int mismatch_cost = 3;
+    int gap_cost      = 2;
+
+    SequenceAlingment alignment(t1, t2, mismatch_cost, gap_cost);
 
     int cost     = alignment.get_minimum_cost();
     auto final_x = alignment.final_x;
