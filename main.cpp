0bd5f84db29a6d02ff536335f3ac99362fb18bff
Yudi Yamane <yudiyamane@gmail.com>
2023-07-11 22:47:10 -0300

    feat: adiciona find_solution

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 
 #include "sequence-alignment.h"
 
-int main() {
-    string t1 = "AGGGCT";
-    string t2 = "AGGCA";
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::printf("Usage: %s \"string1\" \"string2\"", argv[0]);
+        exit(2);
+    }
+
+    // string t1 = "AGGGCT";
+    // string t2 = "AGGCA";
+    string t1 = argv[1];
+    string t2 = argv[2];
     SequenceAlingment alignment(t1, t2, 3, 2);
 
-    std::cout << "Min cost " << alignment.get_minimum_cost() << std::endl;
+    int cost     = alignment.get_minimum_cost();
+    auto final_x = alignment.final_x;
+    auto final_y = alignment.final_y;
+
+    cout << "Min cost " << cost << endl;
+    cout << "X: " << final_x << endl;
+    cout << "Y: " << final_y << endl;
 }
--- sequence-alignment.h
+++ sequence-alignment.h
@@ -22,8 +22,7 @@ public:
         int size = n + m + 1;
         costs.resize(size);
         for (int i = 0; i < size; i++) {
-            costs[i] = vector<int>();
-            costs[i].resize(size);
+            costs[i] = vector<int>(size, 0);
         }
 
         for (int i = 0; i <= (n + m); i++) {
@@ -32,11 +31,14 @@ public:
         }
 
         min_cost = calculate_minimum_cost();
+        find_solution();
     }
 
     int get_minimum_cost() {
         return min_cost;
     };
+
+    string final_x, final_y;
 
 private:
     int min_cost = 0;
@@ -59,5 +61,68 @@ private:
         }
 
         return costs[m][n];
+    }
+
+    void find_solution() {
+        int size = n + m;
+        int i = m, j = n;
+
+        int x_pos = size;
+        int y_pos = size;
+
+        auto x_tmp = vector<int>(size + 1);
+        auto y_tmp = vector<int>(size + 1);
+
+        while (!(i == 0 || j == 0)) {
+            if (x[i - 1] == y[j - 1]) {
+                x_tmp[x_pos--] = (int)x[i - 1];
+                y_tmp[y_pos--] = (int)y[j - 1];
+                i--;
+                j--;
+            } else if (costs[i - 1][j - 1] + mismatch_cost == costs[i][j]) {
+                x_tmp[x_pos--] = (int)x[i - 1];
+                y_tmp[y_pos--] = (int)y[j - 1];
+                i--;
+                j--;
+            } else if (costs[i - 1][j] + gap_cost == costs[i][j]) {
+                x_tmp[x_pos--] = (int)x[i - 1];
+                y_tmp[y_pos--] = (int)'_';
+                i--;
+            } else if (costs[i][j - 1] + gap_cost == costs[i][j]) {
+                x_tmp[x_pos--] = (int)'_';
+                y_tmp[y_pos--] = (int)y[j - 1];
+                j--;
+            }
+        }
+
+        while (x_pos > 0) {
+            if (i > 0)
+                x_tmp[x_pos--] = (int)x[--i];
+            else
+                x_tmp[x_pos--] = (int)'_';
+        }
+        while (y_pos > 0) {
+            if (j > 0)
+                y_tmp[y_pos--] = (int)y[--j];
+            else
+                y_tmp[y_pos--] = (int)'_';
+        }
+
+        int id = 1;
+        for (i = size; i >= 1; i--) {
+            if ((char)y_tmp[i] == '_' && (char)x_tmp[i] == '_') {
+                id = i + 1;
+                break;
+            }
+        }
+
+        string x_final, y_final;
+        for (i = id; i <= size; i++)
+            x_final += (char)x_tmp[i];
+        for (i = id; i <= size; i++)
+            y_final += (char)y_tmp[i];
+
+        final_x = x_final;
+        final_y = y_final;
     }
 };
